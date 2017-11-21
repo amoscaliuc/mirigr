b7123904fcbc0d50a79f06ea5d08e60687bec2a3
amoscaliuc <amoscaliuc@gmail.com>
2017-11-21 17:34:02 +0200

    portfolio added new items

--- database/migrations/2017_11_18_110808_create_board_games_table.php
+++ database/migrations/2017_11_18_110808_create_board_games_table.php
@@ -53,7 +53,7 @@ class CreateBoardGamesTable extends Migration
         $now = \Carbon\Carbon::now();
         return [
             [
-                "name" => "Kingdom Builder (Строитель Королевства)",
+                "name" => "Строитель Королевства",
                 "description" => "
                     Перед Вами красивая градостроительная игра с простыми правилами и интересным игровым процессом. В 2012 году Kingdom Builder признана лучшей игрой года в самой престижной настольной премии Spiel des Jahres, так что на нее стоит обратить внимание!<br>
                     Участники настольной игры Kingdom Builder будут застраивать собственные королевства: занимать земли и устанавливать на них поселения, которые, в свою очередь, будут приносить королевству прибыль. Владелец самого богатого королевства и станет победителем игры.
@@ -71,7 +71,7 @@ class CreateBoardGamesTable extends Migration
                 "updated_at" => $now
             ],
             [
-                "name" => "Istanbul:  Brief & Siegel + Mocha & Baksheesh (Станбул: Письма и Печати + Мокко и Бакшиш)",
+                "name" => "Станбул: Письма и Печати + Мокко и Бакшиш",
                 "description" => "
                     Перед вами хорошая экономическая стратегия с внушительным списком наград. Вы ищите что-то особенное в игре, которое отличает ее от других - вы это найдете в игре Istanbul. Несложные правила, интересная механика, красивое оформление и хорошая реиграбельность - все это про настольную игру Istanbul. 
                 ",
@@ -84,6 +84,40 @@ class CreateBoardGamesTable extends Migration
                 "image" => "istanbul.jpg",
                 "tesera_url" => "http://tesera.ru/game/Istanbul/",
                 "youtube_url" => "https://www.youtube.com/embed/Cpx7dtQhQyQ",
+                "created_at" => $now,
+                "updated_at" => $now
+            ],
+            [
+                "name" => "Пять Племен: The Artisans of Naqala + Whims Of The Sultan",
+                "description" => "
+                    Пять племен – экономическая стратегия, в которой есть все: аукцион, планирование, взаимодействие, сбор сетов, перемещение рабочих, очки за все, что происходит в игре и элегантная механика. Экономическая евро-стратегия, в которой предстоит вступить в предвыборную борьбу за кресло Султана.  
+                ",
+                "type" => 2,
+                "number_of_players" => "2-4",
+                "age" => "от 13+",
+                "publish_year" => "2014",
+                "time" => "~60+",
+                "language" => "Языконезависимая",
+                "image" => "five_tribes.jpg",
+                "tesera_url" => "http://tesera.ru/game/5tribes/",
+                "youtube_url" => "https://www.youtube.com/embed/wBxJurJO3Og",
+                "created_at" => $now,
+                "updated_at" => $now
+            ],
+            [
+                "name" => "7 Чудес: Города + Вавилон",
+                "description" => "
+                    Настольная игра 7 Чудес (7 Wonders) очередная веха в эволюции настольных игр. Эдакая изящная Цивилизация лайт в хорошем смысле слова. Механика игры построена на драфте (выбираем карту из набора и передаем соседу, при этом получаем от другого соседа его набор карт - и так по кругу).  
+                ",
+                "type" => 1,
+                "number_of_players" => "2-7",
+                "age" => "от 10+",
+                "publish_year" => "2010",
+                "time" => "~40+",
+                "language" => "Языконезависимая",
+                "image" => "7wonders.jpg",
+                "tesera_url" => "http://tesera.ru/game/7_wonders/",
+                "youtube_url" => "https://www.youtube.com/embed/sutpd1jYg00",
                 "created_at" => $now,
                 "updated_at" => $now
             ]
